1fda01b5a8ffa9e7eeaae4f69df1f5d898a68e19
Alina Vakulenko <114286140+alina-vakulenko@users.noreply.github.com>
2022-12-07 22:26:42 +0200

    prepared basic app structure
    imported axios, bootstrap, added fontawesome

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>
+          <h1>Word Explorer</h1>
+        </header>
+        <main className="main-section">
+          <form>
+            <input
+              type="search"
+              placeholder="Enter a keyword..."
+              autoFocus="on"
+              className="form-control"
+            ></input>
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-warning search-button"
+            ></input>
+          </form>
+          <section>Pronunciation</section>
+          <section>Definition</section>
+        </main>
+        <footer>
+          <a href="/" target="_blank" rel="noopener norefferrer" title="GitHub">
+            Open-source
+          </a>{" "}
+          code using React by{" "}
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener norefferrer"
+            title="Portfolio website"
+          >
+            Alina Vakulenko
+          </a>
+          <p className="social-links">
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener norefferrer"
+              title="GitHub"
+            >
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener norefferrer"
+              title="LinkedIn"
+            >
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener norefferrer"
+              title="Telegram"
+            >
+              <i class="fa-brands fa-telegram"></i>
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
